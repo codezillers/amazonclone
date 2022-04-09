8795fe47e23b94d5ca27eccac1e2f13e76b708f9
Ashrof Salman <salmanashraf853@gmil.com>
2022-04-09 15:22:34 +0100

    feat(categories): adding more categories section

---
+++ src/components/HorizontalCategory.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const HorizontalCategory = ({heading}) => {
+    return (
+      <div className="horiz-category">
+        <h3>{heading}</h3>
+        <div className="list">
+          <div className="item">
+            <img
+              src="https://m.media-amazon.com/images/I/71l4BLMaH7S._AC_SY400_.jpg"
+              alt=""
+            />
+          </div>
+          <div className="item">
+            <img
+              src="https://m.media-amazon.com/images/I/619gB6FwQNL._AC_SY400_.jpg"
+              alt=""
+            />
+          </div>
+          <div className="item">
+            <img
+              src="https://m.media-amazon.com/images/I/71EP8ds034L._AC_SY400_.jpg"
+              alt=""
+            />
+          </div>
+          <div className="item">
+            <img
+              src="https://m.media-amazon.com/images/I/41Fl8N373eL._AC_SY400_.jpg"
+              alt=""
+            />
+          </div>
+          <div className="item">
+            <img
+              src="https://m.media-amazon.com/images/I/71fdZ-Ei0tL._AC_SY400_.jpg"
+              alt=""
+            />
+          </div>
+          <div className="item">
+            <img
+              src="https://m.media-amazon.com/images/I/71fdZ-Ei0tL._AC_SY400_.jpg"
+              alt=""
+            />
+          </div>
+          <div className="item">
+            <img
+              src="https://m.media-amazon.com/images/I/61yoN+jt5tL._AC_SY400_.jpg"
+              alt=""
+            />
+          </div>
+          <div className="item">
+            <img
+              src="https://m.media-amazon.com/images/I/61+WZN3BbKS._AC_SY400_.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default HorizontalCategory;
--- src/pages/LandingPage/Categories.js
+++ src/pages/LandingPage/Categories.js
@@ -1,69 +1,106 @@
 import React from "react";
 import CategoryCard from "../../components/CategoryCard";
+import HorizontalCategory from "../../components/HorizontalCategory";
 
 const Categories = () => {
   return (
-    <div className="categories">
-      <CategoryCard
-        heading={"Gaming accessories"}
-        btnTitle={"See more"}
-        image={[
-          "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Headset_2x._SY232_CB667159060_.jpg",
-          "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Keyboard_2x._SY232_CB667159063_.jpg",
-          "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Mouse_2x._SY232_CB667159063_.jpg",
-          "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Chair_2x._SY232_CB667159060_.jpg",
-        ]}
-        multipleImage
-      />
-      <CategoryCard
-        heading={"Shop by Category"}
-        btnTitle={"Shop now"}
-        image={[
-          "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/February/Dashboard/computer240x._SY170_CB468850970_.jpg",
-          "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/August/DashboardCard/PS4_240X._SY170_CB438749318_.jpg",
-          "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/February/Dashboard/Baby120X._SY85_CB468850882_.jpg",
-          "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/February/Dashboard/Toys240X._SY170_CB468851693_.jpg",
-        ]}
-        multipleImage
-      />
-      <CategoryCard
-        heading={"Health & Personal Care"}
-        btnTitle={"Shop now"}
-        image={
-          "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2022/February/DashboardCards/GW_CONS_AUS_HPC_HPCEssentials_CatCard_Desktop2x._SY608_CB627424361_.jpg"
-        }
-      />
-      <div></div>
+    <div>
+      <div className="categories">
+        <CategoryCard
+          heading={"Gaming accessories"}
+          btnTitle={"See more"}
+          image={[
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Headset_2x._SY232_CB667159060_.jpg",
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Keyboard_2x._SY232_CB667159063_.jpg",
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Mouse_2x._SY232_CB667159063_.jpg",
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_Quad_Chair_2x._SY232_CB667159060_.jpg",
+          ]}
+          multipleImage
+        />
+        <CategoryCard
+          heading={"Shop by Category"}
+          btnTitle={"Shop now"}
+          image={[
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/February/Dashboard/computer240x._SY170_CB468850970_.jpg",
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/August/DashboardCard/PS4_240X._SY170_CB438749318_.jpg",
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/February/Dashboard/Baby120X._SY85_CB468850882_.jpg",
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/February/Dashboard/Toys240X._SY170_CB468851693_.jpg",
+          ]}
+          multipleImage
+        />
+        <CategoryCard
+          heading={"Health & Personal Care"}
+          btnTitle={"Shop now"}
+          image={
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2022/February/DashboardCards/GW_CONS_AUS_HPC_HPCEssentials_CatCard_Desktop2x._SY608_CB627424361_.jpg"
+          }
+        />
+        <div></div>
 
-      <CategoryCard
-        heading={"Dresses"}
-        btnTitle={"Shop now"}
-        image={
-          "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2022/February/DashboardCards/Fuji_dash_dress_2X._SY608_CB626369146_.jpg"
-        }
-      />
-      <CategoryCard
-        heading={"Shop activity trackers and smartwatches"}
-        btnTitle={"Shop now"}
-        image={
-          "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_SmartWatch_1X._SY304_CB639922137_.jpg"
-        }
-      />
-      <CategoryCard
-        heading={"For your Fitness Needs"}
-        btnTitle={"Shop now"}
-        image={
-          "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_Fitness_1X._SY304_CB639748186_.jpg"
-        }
-      />
-      <CategoryCard
-        heading={"Computers & Accessories"}
-        btnTitle={"Shop now"}
-        image={
-          "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_PC_2x._SY608_CB431800964_.jpg"
-        }
-          />
-          
+        <CategoryCard
+          heading={"Dresses"}
+          btnTitle={"Shop now"}
+          image={
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2022/February/DashboardCards/Fuji_dash_dress_2X._SY608_CB626369146_.jpg"
+          }
+        />
+        <CategoryCard
+          heading={"Shop activity trackers and smartwatches"}
+          btnTitle={"Shop now"}
+          image={
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_SmartWatch_1X._SY304_CB639922137_.jpg"
+          }
+        />
+        <CategoryCard
+          heading={"For your Fitness Needs"}
+          btnTitle={"Shop now"}
+          image={
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_Fitness_1X._SY304_CB639748186_.jpg"
+          }
+        />
+        <CategoryCard
+          heading={"Computers & Accessories"}
+          btnTitle={"Shop now"}
+          image={
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_PC_2x._SY608_CB431800964_.jpg"
+          }
+        />
+      </div>
+      <HorizontalCategory />
+      <div className="categories" style={{ marginTop: 0 }}>
+        <CategoryCard
+          heading={"Health & Personal Care"}
+          btnTitle={"Shop now"}
+          image={
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_Toys_2X._SY608_CB639759849_.jpg"
+          }
+        />
+        <CategoryCard
+          heading={"Gaming accessories"}
+          btnTitle={"See more"}
+          image={[
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_WomenFashion_Sweatshirt_Quad_Cat_2x._SY232_CB418609084_.jpg",
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_WomenFashion_Joggers_Quad_Cat_2x._SY232_CB418608748_.jpg",
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_WomenFashion_Cardigans_Quad_Cat_2x._SY232_CB418608722_.jpg",
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_WomenFashion_Tees_Quad_Cat_2x._SY232_CB418608878_.jpg",
+          ]}
+          multipleImage
+        />
+        <CategoryCard
+          heading={"Dresses"}
+          btnTitle={"Shop now"}
+          image={
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_Pets_2X._SY608_CB639746708_.jpg"
+          }
+        />
+        <CategoryCard
+          heading={"Dresses"}
+          btnTitle={"Shop now"}
+          image={
+            "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_Pets_2X._SY608_CB639746708_.jpg"
+          }
+        />
+      </div>
     </div>
   );
 };
