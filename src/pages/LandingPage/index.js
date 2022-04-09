fdbfa564639d579bc6ecfb5d838fae2b14f18ac7
Ashrof Salman <salmanashraf853@gmil.com>
2022-04-09 13:08:54 +0100

    feat(carousel): add slider courosel

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+const landing1 = require("../../assets/landing1.jpg");
+const landing2 = require("../../assets/landing2.jpg");
+const landing3 = require("../../assets/landing3.jpg");
+const landing4 = require("../../assets/landing4.jpg");
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
       <div className="container">
-        <p>page content</p>
+        <Carousel autoPlay showThumbs={false} showArrows={false} showStatus={false}>
+          <div>
+            <img src={landing1} alt="" />
+          </div>
+          <div>
+            <img src={landing2} alt="" />
+          </div>
+          <div>
+            <img src={landing3} alt="" />
+          </div>
+          <div>
+            <img src={landing4} alt="" />
+          </div>
+        </Carousel>
       </div>
     </div>
   );
