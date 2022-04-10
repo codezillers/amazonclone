08b42eda7310d78bdedf13e79cfd217e230595f1
Muhammed Shittu <coolzyte@gmail.com>
2022-04-10 02:10:57 +0100

    Done with header section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { SearchIcon, ShoppingCartIcon } from "@heroicons/react/outline";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="top">
+      <div className="top-nav">
         <div className="logo">
           <img
             src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
@@ -17,7 +21,7 @@ const Header = () => {
           <SearchIcon className="search-icon" />
         </div>
 
-        <div className="top-right">
+        <div className="top-right flex">
           <div className="items">
             <p>Hello, Ashrof</p>
             <p>Account & Lists</p>
@@ -28,23 +32,28 @@ const Header = () => {
             <p>& Orders</p>
           </div>
 
-          <div
-            className="items"
-            style={{
-              display: "flex",
-              alignItem: "center",
-              justifyContent: "center",
-              position: "relative",
-            }}
-          >
-            <span className="basket-count">0</span>
-            <ShoppingCartIcon style={{ height: "30" }} />
-            <p className="basket-text">Basket</p>
+          <div className="items flex relative">
+            <span className="nav-cart-count absolute">0</span>
+            <ShoppingCartIcon style={{ height: "30", marginTop: "12" }} />
+            <p className="basket-text hidden">Cart</p>
           </div>
         </div>
       </div>
 
-      <div></div>
+      <div className="bottom-nav">
+        <p className="menu-icon flex">
+          <MenuIcon style={{ height: "24px", marginRight: "4px" }} />
+          All
+        </p>
+        <p className="link">Today's Deals</p>
+        <p className="link">Browsing History</p>
+        <p className="link">Buy Again</p>
+
+        <p className="link hidden">Customer Service</p>
+        <p className="link hidden">Gift Cards</p>
+        <p className="link hidden">Registry</p>
+        <p className="link hidden">Sell</p>
+      </div>
     </header>
   );
 };
